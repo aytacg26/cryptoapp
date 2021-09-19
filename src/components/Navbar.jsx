a860fc080658af7c9352e8da63a263b87d0ffe7e
aytacguley <aytacguley@dklassix.com>
2021-09-19 22:20:59 +0300

    Keys added to menu items

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,16 +21,16 @@ const Navbar = () => {
         {/* <Button className="menu-control-container"></Button> */}
       </div>
       <Menu theme='dark'>
-        <Menu.Item icon={<HomeOutlined />}>
+        <Menu.Item icon={<HomeOutlined />} key='home'>
           <Link to='/'>Home</Link>
         </Menu.Item>
-        <Menu.Item icon={<FundOutlined />}>
+        <Menu.Item icon={<FundOutlined />} key='cryptocurrencies'>
           <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
         </Menu.Item>
-        <Menu.Item icon={<MoneyCollectOutlined />}>
+        <Menu.Item icon={<MoneyCollectOutlined />} key='exchanges'>
           <Link to='/exchanges'>Exchanges</Link>
         </Menu.Item>
-        <Menu.Item icon={<BulbOutlined />}>
+        <Menu.Item icon={<BulbOutlined />} key='news'>
           <Link to='/news'>News</Link>
         </Menu.Item>
       </Menu>
