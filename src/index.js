434b8534604d85dde55ec1ae979f9133885b4d25
aytacguley <aytacguley@dklassix.com>
2021-09-19 22:21:46 +0300

    Provider and store added to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './App';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <Router>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </Router>,
   document.getElementById('root')
 );
