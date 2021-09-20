a225b5f8126a574277dd2e570ec9806ff769f372
aytacguley <aytacguley@dklassix.com>
2021-09-21 00:01:43 +0300

    key added to coin name options

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -43,7 +43,9 @@ const News = ({ simplified }) => {
             >
               <Option value='Cryptocurrency'>All Crypto News</Option>
               {data?.data?.coins.map((coin) => (
-                <Option value={coin.name}>{coin.name}</Option>
+                <Option value={coin.name} key={coin.name}>
+                  {coin.name}
+                </Option>
               ))}
             </Select>
           </Col>
