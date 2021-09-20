e4ba3c892352e6a67467351bb3f5a81636bdfb27
aytacguley <aytacguley@dklassix.com>
2021-09-21 00:00:03 +0300

    Exchanges Page component created

--- src/components/Exchanges.jsx
+++ src/components/Exchanges.jsx
@@ -1,7 +1,62 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import millify from 'millify';
+import { Collapse, Row, Col, Typography, Avatar } from 'antd';
+import HTMLReactParser from 'html-react-parser';
+
+import { useGetExchangesQuery } from '../services/cryptoApi';
+import Loader from './UI/Loader/Loader';
+
+const { Text } = Typography;
+const { Panel } = Collapse;
 
 const Exchanges = () => {
-  return <div>Exchanges</div>;
+  const { data } = useGetExchangesQuery();
+  const exchangesList = data?.data?.exchanges;
+
+  if (!data) return <Loader />;
+
+  return (
+    <Fragment>
+      <Row>
+        <Col span={6}>Exchanges</Col>
+        <Col span={6}>24h Trade Volume</Col>
+        <Col span={6}>Markets</Col>
+        <Col span={6}>Change</Col>
+      </Row>
+      <Row>
+        {exchangesList.map((exchange) => (
+          <Col span={24} key={exchange.id}>
+            <Collapse>
+              <Panel
+                showArrow={false}
+                header={
+                  <Row>
+                    <Col span={6}>
+                      <Text>
+                        <strong>{exchange.rank}.</strong>
+                      </Text>
+                      <Avatar
+                        className='exchange-image'
+                        src={exchange.iconUrl}
+                      />
+                      <Text>
+                        <strong>{exchange.name}</strong>
+                      </Text>
+                    </Col>
+                    <Col span={6}>${millify(exchange.volume)}</Col>
+                    <Col span={6}>{millify(exchange.numberOfMarkets)}</Col>
+                    <Col span={6}>{millify(exchange.marketShare)}%</Col>
+                  </Row>
+                }
+              >
+                {HTMLReactParser(exchange.description || '')}
+              </Panel>
+            </Collapse>
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
 };
 
 export default Exchanges;
