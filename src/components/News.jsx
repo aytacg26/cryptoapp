af3f40fe64e080224a35a1b417422144fe7fbd12
aytacguley <aytacguley@dklassix.com>
2021-09-20 20:41:07 +0300

    Crypto News page completed

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,7 +1,106 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
+import moment from 'moment';
+import Loader from '../components/UI/Loader/Loader';
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
-const News = () => {
-  return <div>News</div>;
+const { Text, Title } = Typography;
+const { Option } = Select;
+const demoImage =
+  'https://www.bing.com/th?id=OVFT.mpzuVZnv8dwIMRfQGPbOPC&pid=News';
+
+const News = ({ simplified }) => {
+  const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
+
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: newsCategory,
+    count: simplified ? 6 : 12,
+  });
+  const { data } = useGetCryptosQuery(100);
+
+  if (!cryptoNews?.value) return <Loader />;
+
+  return (
+    <Fragment>
+      {!simplified && (
+        <Title level={2} className='heading' style={{ marginBottom: 35 }}>
+          Cryptocurrency News
+        </Title>
+      )}
+      <Row gutter={[24, 24]}>
+        {!simplified && (
+          <Col span={24}>
+            <Select
+              showSearch
+              className='select-news'
+              placeholder='Select a Crypto'
+              optionFilterProp='children'
+              onChange={(value) => setNewsCategory(value)}
+              filterOption={(input, option) =>
+                option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+              }
+            >
+              <Option value='Cryptocurrency'>All Crypto News</Option>
+              {data?.data?.coins.map((coin) => (
+                <Option value={coin.name}>{coin.name}</Option>
+              ))}
+            </Select>
+          </Col>
+        )}
+
+        {cryptoNews.value.map((news) => (
+          <Col xs={24} sm={12} lg={8} key={news.name}>
+            <Card hoverable className='news-card'>
+              <a
+                href={news.url}
+                target='_blank'
+                referrerPolicy='no-referrer'
+                rel='noreferrer'
+              >
+                <div className='news-image-container'>
+                  <Title className='news-title' level={4}>
+                    {news.name}
+                  </Title>
+                  <img
+                    src={news?.image?.thumbnail?.contentUrl || demoImage}
+                    alt={news.name}
+                    style={{
+                      maxHeight: '100px',
+                      borderRadius: '5px',
+                      boxShadow: '1px 1px 3px black',
+                    }}
+                  />
+                </div>
+                <p>
+                  {news.description.length > 100
+                    ? `${news.description.substring(0, 100)}...`
+                    : news.description}
+                </p>
+                <div className='provider-container'>
+                  <div>
+                    <Avatar
+                      src={
+                        news.provider[0]?.image?.thumbnail?.contentUrl ||
+                        demoImage
+                      }
+                      alt='News'
+                    />
+                    <Text className='provider-name'>
+                      {news.provider[0]?.name}
+                    </Text>
+                  </div>
+                  <Text>
+                    {moment(news.datePublished).startOf('ss').fromNow()}
+                  </Text>
+                </div>
+              </a>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Fragment>
+  );
 };
 
 export default News;
